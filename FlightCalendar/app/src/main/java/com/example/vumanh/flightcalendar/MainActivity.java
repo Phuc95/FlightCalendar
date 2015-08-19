0c64f4388cb35e22b0f17ace9b5911e6395c9313
Phuc95 <ducphuc9555@gmail.com>
2015-08-19 14:05:37 +0700

    Change compileSdk

--- FlightCalendar/app/src/main/java/com/example/vumanh/flightcalendar/MainActivity.java
+++ FlightCalendar/app/src/main/java/com/example/vumanh/flightcalendar/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.vumanh.flightcalendar;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
